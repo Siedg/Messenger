8606601fcb01cfa0763b1f3ad0aac3c87037a75b
Guilherme <siedg104@gmail.com>
2020-06-25 15:29:59 -0300

    Registration is now working properly

--- app/src/main/java/com/siedg/messenger/MainActivity.kt
+++ app/src/main/java/com/siedg/messenger/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +12,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val email = email_edittext_registerscreen.toString()
-        val password = password_edittext_registerscreen.toString()
 
         register_button_registerscreen.setOnClickListener {
+            val email = email_edittext_registerscreen.text.toString()
+            val password = password_edittext_registerscreen.text.toString()
             Log.d("MainActivity", "Email: $email")
             Log.d("MainActivity", "Password: $password")
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+
+                    Log.d(
+                        "MainActivity",
+                        "Successfully created user with uid: ${it.result!!.user!!.uid}"
+                    )
+                }
         }
 
         alreadyhaveaccount_textview_registerscreen.setOnClickListener {
