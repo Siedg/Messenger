8ef35d5aa86e25f96be4ec5eb7f6c5bafa645297
Guilherme <siedg104@gmail.com>
2020-06-25 14:39:22 -0300

    Added text fields and button

--- app/src/main/java/com/siedg/messenger/MainActivity.kt
+++ app/src/main/java/com/siedg/messenger/MainActivity.kt
@@ -2,10 +2,21 @@ package com.siedg.messenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val email = email_edittext_registerscreen.toString()
+        val password = password_edittext_registerscreen.toString()
+
+
+        register_button_registerscreen.setOnClickListener {
+            Log.d("MainActivity", "Email: $email")
+            Log.d("MainActivity", "Password: $password")
+        }
     }
 }
