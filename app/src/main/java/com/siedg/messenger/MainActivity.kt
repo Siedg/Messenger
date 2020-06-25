499d11f22cb15c9d38f26b61fd470737620019b3
Guilherme <siedg104@gmail.com>
2020-06-25 14:57:05 -0300

    Added login screen

--- app/src/main/java/com/siedg/messenger/MainActivity.kt
+++ app/src/main/java/com/siedg/messenger/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.siedg.messenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -17,6 +18,12 @@ class MainActivity : AppCompatActivity() {
         register_button_registerscreen.setOnClickListener {
             Log.d("MainActivity", "Email: $email")
             Log.d("MainActivity", "Password: $password")
+        }
+
+        alreadyhaveaccount_textview_registerscreen.setOnClickListener {
+            Log.d("MainActivity", "Try to show login activity")
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
