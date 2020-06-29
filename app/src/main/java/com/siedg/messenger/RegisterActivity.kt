486c53bb7573edc5388115c838876c0af4cc9b9e
Guilherme <siedg104@gmail.com>
2020-06-29 12:05:45 -0300

    Photo selector now shows image

--- app/src/main/java/com/siedg/messenger/RegisterActivity.kt
+++ app/src/main/java/com/siedg/messenger/RegisterActivity.kt
@@ -1,8 +1,11 @@
 package com.siedg.messenger
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
@@ -29,6 +32,19 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_PICK)
             intent.type = "image/*"
             startActivityForResult(intent, 0)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            Log.d("RegisterActivity", "Photo was selectet")
+
+            val uri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            selectphoto_button_registerscreen.setBackgroundDrawable(bitmapDrawable)
         }
     }
 
