a4087c63806b479a28794725f5438aabd6af4e31
Guilherme <siedg104@gmail.com>
2020-06-29 16:38:24 -0300

    Refactor

--- app/src/main/java/com/siedg/messenger/messages/NewMessageActivity.kt
+++ app/src/main/java/com/siedg/messenger/messages/NewMessageActivity.kt
@@ -9,7 +9,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.siedg.messenger.R
-import com.siedg.messenger.registerlogin.User
+import com.siedg.messenger.models.User
 import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
