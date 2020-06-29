5c3d2cfbeb390818b610a5366a53122846a71012
Guilherme <siedg104@gmail.com>
2020-06-29 15:42:38 -0300

    Added NewMessageActivity.kt

---
+++ app/src/main/java/com/siedg/messenger/NewMessageActivity.kt
@@ -0,0 +1,20 @@
+package com.siedg.messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_new_message.*
+
+class NewMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_message)
+
+        supportActionBar?.title = "Select User"
+
+        recyclerview_newmesaage.adapter
+        recyclerview_newmesaage.layoutManager = LinearLayoutManager(this)
+
+    }
+}
