042b09c62abac4f2a3953751503f1cd4c506fb6f
Guilherme <siedg104@gmail.com>
2020-06-29 17:30:10 -0300

    Fixed login button

--- app/src/main/java/com/siedg/messenger/registerlogin/LoginActivity.kt
+++ app/src/main/java/com/siedg/messenger/registerlogin/LoginActivity.kt
@@ -1,24 +1,34 @@
 package com.siedg.messenger.registerlogin
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.siedg.messenger.R
+import com.siedg.messenger.messages.LastestMessagesActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("LoginActivity", "=====")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val email = email_edittext_loginscreen.text.toString()
-        val password = password_edittext_loginscreen.text.toString()
 
-       // FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-            //.addOnCompleteListener {}
-            //.addOnFailureListener {}
+
+        login_button_loginscreen.setOnClickListener {
+            val email = email_edittext_loginscreen.text.toString()
+            val password = password_edittext_loginscreen.text.toString()
+            Log.d("LoginActivity", "Email: $email \nPassword: $password \n\n")
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    val intent = Intent(this, LastestMessagesActivity::class.java)
+                    startActivity(intent)
+                }
+                .addOnFailureListener {}
+        }
+
+
+
     }
 }
-
-asdasdas
