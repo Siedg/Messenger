4d28ed4685d4d6de5372078589ed04c8e1f66e44
Guilherme <siedg104@gmail.com>
2020-06-29 16:06:46 -0300

    Added fetchUser

--- app/src/main/java/com/siedg/messenger/NewMessageActivity.kt
+++ app/src/main/java/com/siedg/messenger/NewMessageActivity.kt
@@ -2,9 +2,19 @@ package com.siedg.messenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.tasks.OnCanceledListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
@@ -14,8 +24,40 @@ class NewMessageActivity : AppCompatActivity() {
 
         supportActionBar?.title = "Select User"
 
+        val adapter = GroupAdapter<RecyclerView.ViewHolder>()
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+
+        fetchUsers()
+
+
         recyclerview_newmesaage.adapter
         recyclerview_newmesaage.layoutManager = LinearLayoutManager(this)
 
+    }
+}
+
+private fun fetchUsers() {
+    val ref = FirebaseDatabase.getInstance().getReference("/users")
+    ref.addListenerForSingleValueEvent(object: ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            snapshot.children.forEach {
+                Log.d("NewMessage", it.toString())
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            TODO("Not yet implemented")
+        }
+    })
+}
+
+class UserItem: Item<GroupieViewHolder>() {
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+
+    }
+    override fun getLayout(): Int {
+        return R.layout.user_row_new_message
     }
 }
